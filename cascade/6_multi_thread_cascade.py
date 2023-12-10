177b22fe94c6862b4abe938a48d7741cc3241bc2
Collegenie-X <47956055+jp798@users.noreply.github.com>
2023-12-11 00:38:52 +0900

    Update 6_multi_thread_cascade.py

--- cascade/6_multi_thread_cascade.py
+++ cascade/6_multi_thread_cascade.py
@@ -1,6 +1,7 @@
 import cv2
 import threading
 import copy
+import time
 
 # Haar Cascade 모델 로드
 obstacle_cascade = cv2.CascadeClassifier('path_to_obstacle_cascade.xml')
@@ -11,20 +12,41 @@ sign_cascade = cv2.CascadeClassifier('path_to_sign_cascade.xml')
 cap = cv2.VideoCapture(0)
 
 # 각 객체를 인식하는 함수
-def detect_obstacle(frame, output):
+def detect_obstacle(frame, output, control_signals):
     obstacles = obstacle_cascade.detectMultiScale(frame, 1.3, 5)
     for (x,y,w,h) in obstacles:
         cv2.rectangle(output,(x,y),(x+w,y+h),(255,0,0),2)
+        # 장애물 감지 시 제어 신호 변경
+        control_signals['obstacle'] = True
 
-def detect_traffic_light(frame, output):
+def detect_traffic_light(frame, output, control_signals):
     traffic_lights = traffic_light_cascade.detectMultiScale(frame, 1.3, 5)
     for (x,y,w,h) in traffic_lights:
         cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),2)
+        # 여기에 신호등 색상 분석 로직 추가 필요
+        control_signals['red_light'] = True  # 예시
 
-def detect_sign(frame, output):
+def detect_sign(frame, output, control_signals):
     signs = sign_cascade.detectMultiScale(frame, 1.3, 5)
     for (x,y,w,h) in signs:
         cv2.rectangle(output,(x,y),(x+w,y+h),(0,0,255),2)
+        # 여기에 표지판 종류 분석 로직 추가 필요
+        control_signals['stop_sign'] = True  # 예시
+
+# 차량 제어 함수
+def control_vehicle(control_signals):
+    if control_signals['obstacle']:
+        # 장애물 회피 로직
+        pass
+    elif control_signals['red_light']:
+        # 신호등에서 멈춤
+        pass
+    elif control_signals['stop_sign']:
+        # 정지 표지판에서 정지
+        time.sleep(3)  # 3초간 정지
+    else:
+        # 직진
+        pass
 
 # 메인 루프
 while True:
@@ -32,29 +54,28 @@ while True:
     if not ret:
         break
 
-    # 스레드에서 사용할 프레임 복사본
+    control_signals = {'obstacle': False, 'red_light': False, 'stop_sign': False}
+
     frame_obstacle = copy.deepcopy(original_frame)
     frame_traffic_light = copy.deepcopy(original_frame)
     frame_sign = copy.deepcopy(original_frame)
 
-    # 스레드 생성 및 실행
-    obstacle_thread = threading.Thread(target=detect_obstacle, args=(frame_obstacle, original_frame))
-    traffic_light_thread = threading.Thread(target=detect_traffic_light, args=(frame_traffic_light, original_frame))
-    sign_thread = threading.Thread(target=detect_sign, args=(frame_sign, original_frame))
+    obstacle_thread = threading.Thread(target=detect_obstacle, args=(frame_obstacle, original_frame, control_signals))
+    traffic_light_thread = threading.Thread(target=detect_traffic_light, args=(frame_traffic_light, original_frame, control_signals))
+    sign_thread = threading.Thread(target=detect_sign, args=(frame_sign, original_frame, control_signals))
 
     obstacle_thread.start()
     traffic_light_thread.start()
     sign_thread.start()
 
-    # 스레드 종료 대기
     obstacle_thread.join()
     traffic_light_thread.join()
     sign_thread.join()
 
-    # 결과 표시
+    control_vehicle(control_signals)
+
     cv2.imshow('Frame', original_frame)
 
-    # 'q'를 누르면 종료
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
